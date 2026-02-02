00be9f15e5fe47bfa039c8bd84991bfdf5c94fe8
tarik8mohamed-ux <tarik8mohamed@gmail.com>
2026-02-01 22:01:30 -0500

    Add files via upload

---
+++ ADC_MOD.h
@@ -0,0 +1,15 @@
+#ifndef ADC_MOD_H
+#define ADC_MOD_H
+
+enum ADC_bm{
+	VMON_bm = 0x10,
+	CMON_bm = 0x11
+	};
+
+void ADC_init(uint8_t mode);
+void ADC_channelSEL(uint8_t AIN_POS);
+float ADC_read(void);
+
+
+
+#endif
